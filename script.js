171155ca924235c9c4100cd81f26662590f3c63f
nimit-pasricha <pasricha.nimit@gmail.com>
2024-06-26 15:37:50 +0400

    Rearrange the code to use IIFEs
    The project is too big to not organize it, but it's too small to
    justify dealing with a bundler so I can't use modules

--- script.js
+++ script.js
@@ -1,60 +1,76 @@
-function createArrayOfAllImages(
-  image1Source,
-  image2Source,
-  image3Source,
-  image4Source,
-  image5Source
-) {
-  const image1 = document.createElement("img");
-  image1.src = image1Source;
+const display = (function () {
+  function getAllImages(
+    image1Source,
+    image2Source,
+    image3Source,
+    image4Source,
+    image5Source
+  ) {
+    const image1 = document.createElement("img");
+    image1.src = image1Source;
 
-  const image2 = document.createElement("img");
-  image2.src = image2Source;
+    const image2 = document.createElement("img");
+    image2.src = image2Source;
 
-  const image3 = document.createElement("img");
-  image3.src = image3Source;
+    const image3 = document.createElement("img");
+    image3.src = image3Source;
 
-  const image4 = document.createElement("img");
-  image4.src = image4Source;
+    const image4 = document.createElement("img");
+    image4.src = image4Source;
 
-  const image5 = document.createElement("img");
-  image5.src = image5Source;
+    const image5 = document.createElement("img");
+    image5.src = image5Source;
 
-  return [image1, image2, image3, image4, image5];
-}
+    return [image1, image2, image3, image4, image5];
+  }
 
-function createArrayOfImagesToDisplay(array, indexToDisplayInFrame) {
-  const centerImage = array[indexToDisplayInFrame];
-
-  // if centerImage is at index 0, leftImageIndex becomes -1
-  // which is fine because at() can use negative index
-  const leftImageIndex = indexToDisplayInFrame - 1;
-  const leftImage = array.at(leftImageIndex);
-
-  // if the center image is the last image,
-  // then the right image should be the first image, i.e. the image at index 0
-  const rightImageIndex =
-    indexToDisplayInFrame + 1 >= array.length ? 0 : indexToDisplayInFrame + 1;
-  const rightImage = array[rightImageIndex];
-
-  return [leftImage, centerImage, rightImage];
-}
-
-function displayImages(indexToDisplayInFrame) {
   const bird = "./images/bird.jpg";
   const coffee = "./images/coffee.jpg";
   const cow = "./images/cow.jpg";
   const houses = "./images/houses.jpg";
   const lion = "./images/lion.jpg";
+  const allImages = getAllImages(bird, coffee, cow, houses, lion);
 
-  const allImages = createArrayOfAllImages(bird, coffee, cow, houses, lion);
-  const imagesToDisplay = createArrayOfImagesToDisplay(
-    allImages,
-    indexToDisplayInFrame
-  );
+  function getImagesToDisplay(indexToDisplayInFrame) {
+    const centerImage = allImages[indexToDisplayInFrame];
 
-  const slidesContainer = document.querySelector(".slides");
-  imagesToDisplay.forEach((image) => slidesContainer.appendChild(image));
-}
+    // if centerImage is at index 0, leftImageIndex becomes -1
+    // which is fine because at() can use negative index
+    const leftImageIndex =
+      indexToDisplayInFrame - 1 < 0
+        ? allImages.length - 1
+        : indexToDisplayInFrame - 1;
+    const leftImage = allImages[leftImageIndex];
 
-displayImages(4);
+    // if the center image is the last image,
+    // then the right image should be the first image, i.e. the image at index 0
+    const rightImageIndex =
+      indexToDisplayInFrame + 1 >= allImages.length
+        ? 0
+        : indexToDisplayInFrame + 1;
+    const rightImage = allImages[rightImageIndex];
+
+    return [leftImage, centerImage, rightImage];
+  }
+
+  function displayImages(indexToDisplayInFrame) {
+    const imagesToDisplay = getImagesToDisplay(indexToDisplayInFrame);
+    const slidesContainer = document.querySelector(".slides");
+    slidesContainer.innerHTML = "";
+    imagesToDisplay.forEach((image) => slidesContainer.appendChild(image));
+  }
+  displayImages(0);
+
+  let imageToDisplayIndex = 0;
+  function goToPreviousImage() {
+    const leftButton = document.querySelector(".left-button");
+    leftButton.addEventListener("click", () => {
+      imageToDisplayIndex =
+        imageToDisplayIndex - 1 < 0
+          ? allImages.length - 1
+          : imageToDisplayIndex - 1;
+      displayImages(imageToDisplayIndex);
+    });
+  }
+  goToPreviousImage();
+})();
